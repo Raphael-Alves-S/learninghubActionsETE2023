206377de1f97621ddcb492b1e58807a0c188a8ac
Raphael Alves <rapha.alves75@gmail.com>
2024-03-23 17:14:34 -0300

    relatorio

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,17 @@ module.exports = defineConfig({
     baseUrl: 'https://serverest.dev',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    },    
   },
+  "reporter": "cypress-multi-reporters",
+  "reporterOptions": {
+    "reporterEnabled": "mochawesome",
+    "mochawesomeReporterOptions": {
+      "reportDir": "test/cypress/reports/mocha",
+      "quiet": true,
+      "overwrite": false,
+      "html": false,
+      "json": true
+    }
+  }
 });
